e71922057c384e5244e316c96aba7cfb286e068f
Moises Gomes <moigomes@gmail.com>
2020-11-02 19:00:04 -0300

    ajuste inclusão empréstimos

--- emprestimos/serializers.py
+++ emprestimos/serializers.py
@@ -1,16 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 
 from emprestimos.models import Emprestimo
-from livros.serializers import LivroSerializer
-from clientes.serializers import ClienteSerializer
 
 
 class EmprestimoSerializer(ModelSerializer):
-    livro = LivroSerializer()
-    cliente = ClienteSerializer()
-
     class Meta:
         model = Emprestimo
         fields = ['id', 'cliente', 'livro', 'data_retirada', 'data_entrega']
-
-
